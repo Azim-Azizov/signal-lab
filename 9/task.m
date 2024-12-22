c939424c3a0ae166243221f01400a3b79c484aef
SV Developer <87415292+Azim-Azizov@users.noreply.github.com>
2024-12-23 00:07:09 +0400

    Optimize task 9

--- 9/task.m
+++ 9/task.m
@@ -1,27 +1,28 @@
+clear;
 clc;
-t= 0:0.1:20;
+t=0:0.1:20;
 x=cos(t);
-noise = randn(size(t));
-y= x+noise;
+noise=randn(size(t));
+y=x+noise;
 figure;
-subplot(211); plot(t,x); title('Origional signal');grid on;
-subplot(212); plot(t,y);title("Noisy signal");grid on;
+subplot(211); plot(t, x); title('Origional Signal'); grid on;
+subplot(212); plot(t, y); title("Noisy Signal"); grid on;
 
-X = fft(x);
-Y= fft(y);
+X=fft(x);
+Y=fft(y);
 N=length(X);
-figure
-subplot(211);stem(0:N-1, abs(X)); grid on; legend('X_k');
+figure;
+subplot(211); stem(0:N-1, abs(X)); grid on; legend('X_k');
 subplot(212); stem(0:N-1, abs(Y)); grid on; legend('Y_k');
-for i=1:length(Y);
-    if abs(Y(i))<50
+
+Yclean=Y;
+for i=1:length(Yclean)
+    if abs(Yclean(i))<50
         Yclean(i)=0;
-    else
-        Yclean(i)=Y(i);
     end
 end
 
 yclean=ifft(Yclean);
-figure
-subplot(211);stem(0:N-1,abs(Yclean));legend('Y_k_noiseless');grid on;
-subplot(212);plot(t,yclean); legend('Y(t) noiseless');grid on;
+figure;
+subplot(211); stem(0:N-1, abs(Yclean)); legend('Y_k_noiseless'); grid on;
+subplot(212); plot(t, yclean); legend('Y(t) noiseless'); grid on;
